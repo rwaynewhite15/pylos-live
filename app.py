f7099b51b6e5df9c78c1f32dd7fd7a0021588097
Robert Wayne White <rwaynewhite15@gmail.com>
2026-05-20 21:22:10 -0400

    Switch eventlet -> gevent+gevent-websocket (eventlet worker missing on Render)

--- app.py
+++ app.py
@@ -28,7 +28,7 @@ from db import _db_conn, _PH, _record_pvp_game
 
 app = Flask(__name__)
 app.config["SECRET_KEY"] = os.environ.get("SECRET_KEY", "pylos-dev-secret")
-socketio = SocketIO(app, async_mode="eventlet", cors_allowed_origins="*")
+socketio = SocketIO(app, async_mode="gevent", cors_allowed_origins="*")
 
 rooms = {}        # room_id -> room dict
 sid_to_room = {}  # sid -> room_id
